e515f8a0917a124c8ada35d1b2086f51e7c2d74c
JrDemiurge <jena040105@gmail.com>
2025-09-24 12:18:45 +0300

    Initial commit

---
+++ src/main/java/net/jrdemiurge/skyarena/SkyArena.java
@@ -0,0 +1,37 @@
+package net.jrdemiurge.skyarena;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(SkyArena.MOD_ID)
+public class SkyArena {
+    public static final String MOD_ID = "skyarena";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public SkyArena(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
---
+++ src/main/java/net/jrdemiurge/skyarena/SkyArenaClient.java
@@ -0,0 +1,24 @@
+package net.jrdemiurge.skyarena;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = SkyArena.MOD_ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = SkyArena.MOD_ID, value = Dist.CLIENT)
+public class SkyArenaClient {
+    public SkyArenaClient(ModContainer container) {
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
