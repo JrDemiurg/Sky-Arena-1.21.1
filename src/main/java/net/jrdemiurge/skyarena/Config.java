b5f545284233f7c19a2e40a7be5660d564aa361a
JrDemiurge <jena040105@gmail.com>
2025-09-26 09:54:17 +0300

    Config toml

--- src/main/java/net/jrdemiurge/skyarena/Config.java
+++ src/main/java/net/jrdemiurge/skyarena/Config.java
@@ -1,36 +1,33 @@
 package net.jrdemiurge.skyarena;
 
-import java.util.List;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Neo's config APIs
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    public static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
+    public static final ModConfigSpec.ConfigValue<String> WIKI_LINK = BUILDER
+            .comment("Description for customizing the sky_arena.json config can be found at the following link")
+            .define("wikiLink", "https://github.com/JrDemiurg/DemisSkyArenaMod/wiki");
 
-    public static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+    public static final ModConfigSpec.BooleanValue REQUIRE_EMPTY_CHEST = BUILDER
+            .comment("""
+                    If true, the reward key can only be used on empty chests.
+                    If false, the chest's contents will be cleared before being filled with loot.""")
+            .define("requireEmptyChest", false);
 
-    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
+    public static final ModConfigSpec.BooleanValue ENABLE_LOSS_MESSAGE_LEAVE = BUILDER
+            .comment("If true, a defeat message will be shown when the player leaves the arena.")
+            .define("enableLossMessageLeave", true);
 
-    // a list of strings that are treated as resource locations for items
-    public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), () -> "", Config::validateItemName);
+    public static final ModConfigSpec.BooleanValue ENABLE_LOSS_MESSAGE_DEATH = BUILDER
+            .comment("If true, a defeat message will be shown when the player dies in battle.")
+            .define("enableLossMessageDeath", true);
+
+    public static final ModConfigSpec.BooleanValue ENABLE_UNCLAIMED_REWARD_MESSAGE = BUILDER
+            .comment("""
+                    If true, when mobs receive the Glowing effect, a message will appear recommending leaving the arena to restart the battle.
+                    The message appears only once per game session.""")
+            .define("enableUnclaimedRewardMessage", true);
 
     static final ModConfigSpec SPEC = BUILDER.build();
-
-    private static boolean validateItemName(final Object obj) {
-        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
-    }
 }
